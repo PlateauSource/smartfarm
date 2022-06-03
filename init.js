9bf3b5ab7ae58c0d6a7105e2158a3b8074d6dd58
PlateauSource <106757539+PlateauSource@users.noreply.github.com>
2022-06-03 14:03:21 +0300

    Update init.js

--- init.js
+++ init.js
@@ -1,16 +1,12 @@
-const queryString = window.location.pathname.split('/')[3];
+const queryString = window.location.pathname.split('/')[3]; // get url pathname (realm name)
 
+    var currentLogo = document.querySelector('.plt-smartfarm-logo') // get current logo
 
-    var logoAlani = document.querySelector('.plt-smartfarm-logo')
-    console.log("deneme")
-    if(queryString === "smartfarm"){
-        console.log(1)
-        logoAlani.src = 'https://plateausource.github.io/smartfarm/img/smartfarm-logo-1.svg';
+    if(queryString === "smartfarm"){ // realm name
+        currentLogo.src = 'https://plateausource.github.io/smartfarm/img/smartfarm-logo-1.svg';
     }
-    
-    if(queryString === "smartfarm2"){
-        logoAlani.src = 'https://plateausource.github.io/smartfarm/img/smartfarm-logo-2.svg';
-        console.log(2)
+    else if(queryString === "smartfarm2"){ // realm name
+        currentLogo.src = 'https://plateausource.github.io/smartfarm/img/smartfarm-logo-2.svg';
     }
 
 
