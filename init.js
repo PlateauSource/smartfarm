4626df7236d2533ab944b56de08a53f84b72da5e
PlateauSource <106757539+PlateauSource@users.noreply.github.com>
2022-06-03 12:07:37 +0300

    Update init.js

--- init.js
+++ init.js
@@ -2,15 +2,15 @@ const queryString = window.location.pathname.split('/')[3];
 
 
     var logoAlani = document.querySelector('.plt-smartfarm-logo')
-    alert("deneme")
+    console.log("deneme")
     if(queryString === "smartfarm"){
-        alert(1)
+        console.log(1)
         logoAlani.src = 'img/smartfarm-logo-1.svg';
     }
     
     if(queryString === "smartfarm2"){
         logoAlani.src = 'img/smartfarm-logo-2.svg';
-        alert(2)
+        console.log(2)
     }
 
 
